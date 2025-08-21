aef6b50632c23cbac88d06e46d3e35dde7ae2fe7
Yuvraj-Achrekar <yuvraj.achrekar@gmail.com>
2025-08-22 01:33:31 +0530

    Home Banner

--- src/app/(route)/page.tsx
+++ src/app/(route)/page.tsx
@@ -1,11 +1,14 @@
 import HeroSection from "@/components/home/heroSection";
+import HomeBanner from "@/components/home/homeBanner";
 import ShopByCategory from "@/components/home/shopByCategory";
+import { Home } from "lucide-react";
 
 const HomePage = () => {
 	return (
 		<div>
 			<HeroSection />
 			<ShopByCategory />
+			<HomeBanner />
 			<div>Test</div>
 		</div>
 	);
---
+++ src/components/home/homeBanner.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const HomeBanner = () => {
+	return (
+		<section className="bg-[#D9D9D9] h-[300px] grid place-items-center">
+			<h2 className="banner-content">
+				At Godhajii, we preserve traditional Indian recipes and deliver
+				handcrafted pickles, papads, chutneys, and snacks made with love, care,
+				and no preservatives.
+			</h2>
+		</section>
+	);
+};
+
+export default HomeBanner;
