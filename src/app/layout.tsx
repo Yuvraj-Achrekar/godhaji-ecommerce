be3f331620bb62c045f072788dca2c6cdca6086f
Yuvraj-Achrekar <yuvraj.achrekar@gmail.com>
2025-10-07 00:22:41 +0530

    font change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 import AuthProvider from "@/providers/authProvider";
 import QueryProvider from "@/providers/queryProvider";
 
-const geistSans = Geist({
-	variable: "--font-geist-sans",
+const montserrat = Montserrat({
+	variable: "--font-montserrat",
 	subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
-	subsets: ["latin"],
+	weight: ["400", "500", "600", "700"],
 });
 
 export const metadata: Metadata = {
@@ -33,8 +29,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+			<body className={`${montserrat.className} antialiased`}>
 				<QueryProvider>
 					<AuthProvider>
 						{children}
--- src/components/home/bestSellers.tsx
+++ src/components/home/bestSellers.tsx
@@ -1,149 +1,152 @@
 import { ChevronRight, Heart, Share2 } from "lucide-react";
 import { Button } from "../ui/button";
+import Image from "next/image";
 
 const categoriesList = [
-  {
-    subject: "Lemon Pickle",
-    description:
-      "Brighten up your plate with Godha Brother's lime pickle! Juicy, fresh and tangy taste.",
-    price: "RS.100",
-    cancelPrice: "RS200",
-    rating: 4.5,
-    ratingCount: 1500,
-    off: "50%",
-  },
-  {
-    subject: "Meat & Fish",
-    description:
-      "Enjoy premium quality meat and fish products, carefully selected for freshness and taste.",
-    price: "RS.150",
-    cancelPrice: "RS250",
-    rating: 4.7,
-    ratingCount: 2500,
-    off: "40%",
-  },
-  {
-    subject: "Dairy & Eggs",
-    description:
-      "Rich and wholesome dairy products including milk, butter, and farm-fresh eggs.",
-    price: "RS.120",
-    cancelPrice: "RS200",
-    rating: 2.5,
-    ratingCount: 1800,
-    off: "30%",
-  },
-  {
-    subject: "Beverages",
-    description:
-      "Refreshing beverages and juices made from natural ingredients, perfect for every occasion.",
-    price: "RS.80",
-    cancelPrice: "RS150",
-    rating: 3.2,
-    ratingCount: 950,
-    off: "45%",
-  },
+	{
+		subject: "Lemon Pickle",
+		description:
+			"Brighten up your plate with Godha Brother's lime pickle! Juicy, fresh and tangy taste.",
+		price: "Rs.100",
+		cancelPrice: "Rs200",
+		category: "Pickle",
+		rating: 4.5,
+		ratingCount: 1500,
+		off: "50%",
+	},
+	{
+		subject: "Meat & Fish",
+		description:
+			"Enjoy premium quality meat and fish products, carefully selected for freshness and taste.",
+		price: "Rs.150",
+		cancelPrice: "Rs250",
+		category: "Pickle",
+		rating: 4.7,
+		ratingCount: 2500,
+		off: "40%",
+	},
+	{
+		subject: "Dairy & Eggs",
+		description:
+			"Rich and wholesome dairy products including milk, butter, and farm-fresh eggs.",
+		price: "Rs.120",
+		cancelPrice: "Rs200",
+		category: "Pickle",
+		rating: 2.5,
+		ratingCount: 1800,
+		off: "30%",
+	},
+	{
+		subject: "Beverages",
+		description:
+			"Refreshing beverages and juices made from natural ingredients, perfect for every occasion.",
+		price: "Rs.80",
+		cancelPrice: "Rs150",
+		category: "Pickle",
+		rating: 3.2,
+		ratingCount: 950,
+		off: "45%",
+	},
 ];
 
 // Format count into 1.5k etc
 const formatCount = (count: number) => {
-  if (count >= 1000) {
-    return (count / 1000).toFixed(1).replace(/\.0$/, "") + "k";
-  }
-  return count.toString();
+	if (count >= 1000) {
+		return (count / 1000).toFixed(1).replace(/\.0$/, "") + "k";
+	}
+	return count.toString();
 };
 
 const BestSeller = () => {
-  return (
-    <section className="section-container1">
-      <h2 className="section-title text-[#666666]">Our BestSellers</h2>
-      <div className="w-20 sm:w-24 h-1 sm:h-2 mt-4 mb-6 rounded-3xl bg-[#b36949]" />
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-center w-full">
-        {categoriesList.map((category, index) => {
-          const totalStars = 5;
-          const filledStars = Math.floor(category.rating);
-          const emptyStars = totalStars - filledStars;
+	return (
+		<section className="section-container">
+			<h2 className="section-title text-[#666666]">Our BestSellers</h2>
+			<div className="w-20 sm:w-24 h-1 sm:h-2 mt-4 mb-6 rounded-3xl bg-[#b36949]" />
+			<div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-center w-full">
+				{categoriesList.map((category, index) => {
+					const totalStars = 5;
+					const filledStars = Math.floor(category.rating);
+					const emptyStars = totalStars - filledStars;
 
-          return (
-            <div
-              key={index}
-              className="flex flex-col justify-between bg-white rounded-xl shadow-lg hover:shadow-2xl transition-transform transform hover:-translate-y-1 p-4 sm:p-6 min-h-[400px] sm:min-h-[450px] lg:min-h-[450px] relative"
-            >
-              <div className="relative">
-                <img
-                  src={"/assets/home/item.png"}
-                  alt={category.subject}
-                  className="w-24 sm:w-28 md:w-32 h-24 sm:h-28 md:h-32 mb-4 object-contain mx-auto"
-                />
+					return (
+						<div
+							key={index}
+							className="flex flex-col justify-between bg-white rounded-lg shadow-lg hover:shadow-2xl transition-transform transform hover:-translate-y-1 p-2 md:p-4 gap-2">
+							<div className="relative">
+								<Image
+									src={"/assets/home/item.png"}
+									height={128}
+									width={128}
+									alt={category.subject}
+									className="w-full h-full aspect-square object-cover rounded-lg"
+								/>
+								<div className="absolute left-2 top-2 bg-red-600 text-white text-xs sm:text-sm font-semibold px-4 py-1 rounded-[30px]">
+									{category.off} OFF
+								</div>
+								<div className="absolute top-2 right-2 flex flex-col gap-2">
+									<div className="w-7 h-7 sm:w-8 sm:h-8 flex items-center justify-center rounded-full bg-white shadow-md cursor-pointer">
+										<Heart size={14} className="text-red-500" />
+									</div>
+									<div className="w-7 h-7 sm:w-8 sm:h-8 flex items-center justify-center rounded-full bg-white shadow-md cursor-pointer">
+										<Share2 size={14} className="text-gray-700" />
+									</div>
+								</div>
+							</div>
 
-                <div className="absolute top-2 right-2 flex flex-col gap-2">
-                  <div className="w-7 h-7 sm:w-8 sm:h-8 flex items-center justify-center rounded-full bg-white shadow-md cursor-pointer">
-                    <Heart size={14} className="text-red-500" />
-                  </div>
-                  <div className="w-7 h-7 sm:w-8 sm:h-8 flex items-center justify-center rounded-full bg-white shadow-md cursor-pointer">
-                    <Share2 size={14} className="text-gray-700" />
-                  </div>
-                </div>
-              </div>
+							<div className="flex flex-col gap-1">
+								<div>
+									<span className="text-xs">{category.category}</span>
+									<h2 className="text-base sm:text-lg md:text-xl font-semibold">
+										{category.subject}
+									</h2>
+								</div>
+								<div>
+									<div className="flex items-center gap-2">
+										<span className="text-sm sm:text-base font-medium text-gray-800">
+											{category.rating}
+										</span>
+										<div className="flex">
+											{[...Array(filledStars)].map((_, i) => (
+												<span
+													key={i}
+													className="text-yellow-400 text-sm sm:text-base">
+													★
+												</span>
+											))}
+											{[...Array(emptyStars)].map((_, i) => (
+												<span
+													key={i}
+													className="text-gray-300 text-sm sm:text-base">
+													★
+												</span>
+											))}
+										</div>
+										<span className="text-xs sm:text-sm text-gray-500">
+											({formatCount(category.ratingCount)})
+										</span>
+									</div>
 
-              <div className="mb-2">
-                <h2 className="text-sm sm:text-base md:text-lg font-semibold">
-                  {category.subject}
-                </h2>
-              </div>
-
-              <p className="text-xs sm:text-sm md:text-base text-gray-600 mb-2">
-                {category.description}
-              </p>
-
-              <div className="flex items-center gap-2 mb-3">
-                <span className="text-sm sm:text-base font-medium text-gray-800">
-                  {category.rating}
-                </span>
-                <div className="flex">
-                  {[...Array(filledStars)].map((_, i) => (
-                    <span
-                      key={i}
-                      className="text-yellow-400 text-sm sm:text-base"
-                    >
-                      ★
-                    </span>
-                  ))}
-                  {[...Array(emptyStars)].map((_, i) => (
-                    <span
-                      key={i}
-                      className="text-gray-300 text-sm sm:text-base"
-                    >
-                      ★
-                    </span>
-                  ))}
-                </div>
-                <span className="text-xs sm:text-sm text-gray-500">
-                  ({formatCount(category.ratingCount)})
-                </span>
-              </div>
-
-              <div className="flex justify-between items-center">
-                <div className="flex items-center gap-2">
-                  <span className="text-sm sm:text-base md:text-lg font-semibold">
-                    {category.price}
-                  </span>
-                  <span className="text-xs sm:text-sm line-through text-gray-400">
-                    {category.cancelPrice}
-                  </span>
-                </div>
-                <span className="absolute top-2 left-2 mt-5 bg-red-600 text-white text-xs sm:text-sm font-semibold px-2 py-1 rounded-[30px]">
-                  {category.off} OFF
-                </span>
-              </div>
-              <Button className="bg-[#b36949] hover:bg-[#b36949] justify-center px-6 sm:px-8 py-3 sm:py-4 text-sm sm:text-lg rounded-[5px] flex items-center mt-3">
-                <span className="font-medium">Add To Cart</span>
-              </Button>
-            </div>
-          );
-        })}
-      </div>
-    </section>
-  );
+									<div className="flex justify-between items-center">
+										<div className="flex items-center gap-2">
+											<span className="text-sm sm:text-base md:text-lg font-light">
+												{category.price}
+											</span>
+											<span className="text-xs sm:text-sm line-through text-gray-400">
+												{category.cancelPrice}
+											</span>
+										</div>
+									</div>
+								</div>
+								<Button className="bg-[#b36949] hover:bg-[#b36949] justify-center px-6 sm:px-8 py-3 sm:py-4 text-sm sm:text-lg rounded-[5px] flex items-center">
+									<span className="font-medium">Add To Cart</span>
+								</Button>
+							</div>
+						</div>
+					);
+				})}
+			</div>
+		</section>
+	);
 };
 
 export default BestSeller;
